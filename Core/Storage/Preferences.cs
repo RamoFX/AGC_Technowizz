952816e44a0b3948fb0159b22775c747727942fb
Roman Dvorovoy <roman.dvorovoy123@gmail.com>
2022-02-02 00:01:11 +0100

    update: renamed base

--- Core/Storage/Preferences.cs
+++ Core/Storage/Preferences.cs
@@ -5,7 +5,7 @@
 namespace Core.Storage {
   static class Preferences {
     static public readonly string LayoutBasePath = Paths.GetDocumentsPath();
-    static public readonly string LayoutBaseName = "AGC_Technowizz_Configs";
+    static public readonly string LayoutBaseName = "AGC Technowizz Layouts";
     static public readonly string LayoutsPath = Paths.JoinPaths(LayoutBasePath, LayoutBaseName);
   }
 }
