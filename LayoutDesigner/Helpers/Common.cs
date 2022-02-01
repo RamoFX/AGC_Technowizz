186c6e144a799057d3a27cf60369760713383c5a
Roman Dvorovoy <roman.dvorovoy123@gmail.com>
2022-02-01 23:29:30 +0100

    feature: switch to another form helper

---
+++ LayoutDesigner/Helpers/Common.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace LayoutDesigner.Helpers {
+  static class Common {
+    static public void SwitchToForm(Form from, Form to) {
+      from.Hide();
+      to.ShowDialog();
+      from.Close();
+    }
+  }
+}
