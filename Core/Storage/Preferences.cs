1c44f15241d14e503e5a9ef06f7090d05c5a6536
Roman Dvorovoy <roman.dvorovoy123@gmail.com>
2022-02-02 00:19:59 +0100

    fixed: do not use spaces in paths...

--- Core/Storage/Preferences.cs
+++ Core/Storage/Preferences.cs
@@ -5,7 +5,7 @@
 namespace Core.Storage {
   public static class Preferences {
     static public readonly string LayoutBasePath = Paths.GetDocumentsPath();
-    static public readonly string LayoutBaseName = "AGC Technowizz Layouts";
+    static public readonly string LayoutBaseName = "AGC_Technowizz_Layouts";
     static public readonly string LayoutsPath = Paths.JoinPaths(LayoutBasePath, LayoutBaseName);
   }
 }
