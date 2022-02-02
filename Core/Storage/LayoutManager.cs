267aac4b7089ac2a55e218ce2a86211a2c1f99cc
Roman Dvorovoy <roman.dvorovoy123@gmail.com>
2022-02-02 01:43:02 +0100

    feature: easier path info gathering

---
+++ Core/Storage/LayoutManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+
+namespace Core.Storage {
+  static public class LayoutManager {
+    static public IEnumerable<string> GetExistingLayoutsPaths() {
+      try {
+        return Directory.GetFiles(Preferences.LayoutsPath);
+      } catch (DirectoryNotFoundException) {
+        return new string[] {};
+      }
+    }
+
+    static public IEnumerable<string> GetExistingLayoutsNames() {
+      return GetExistingLayoutsPaths().Select(path => Path.GetFileNameWithoutExtension(Path.GetFileName(path)));
+    }
+
+    static bool IsNameAlreadyTaken(string name) {
+      return GetExistingLayoutsNames().Contains(name);
+    }
+  }
+}
