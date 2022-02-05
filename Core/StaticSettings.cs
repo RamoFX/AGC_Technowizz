c32d4475ff93943a2a99e65f87590c1ccbfb8149
Roman Dvorovoy <roman.dvorovoy123@gmail.com>
2022-02-05 21:34:06 +0100

    refactor: Colors have it's own file

--- Core/StaticSettings.cs
+++ Core/StaticSettings.cs
@@ -15,34 +15,34 @@ namespace Core {
 
     // Layout
     public const           string LayoutsBaseName = "Layouts";
-    static public readonly string LayoutsPath     = Path.Combine(SolutionPath, LayoutsBaseName);
+    static public readonly string     LayoutsPath = Path.Combine(SolutionPath, LayoutsBaseName);
 
 
 
     // Visual
-    public const           int   UnitSize                     = 30;
-    public const           int   OutlineSize                  = 4;
+    public const           int                        UnitSize = 30;
+    public const           int                     OutlineSize = 4;
 
-    static public readonly Color LayoutOutlineColor           = Color.DarkGray;
-    static public readonly Color LayoutGridColor              = Color.LightGray;
+    static public readonly Color            LayoutOutlineColor = Color.DarkGray;
+    static public readonly Color               LayoutGridColor = Color.LightGray;
 
-    static public readonly Color ZoneStorageOutlineColor      = Color.SkyBlue;
-    static public readonly Color ZoneOtherOutlineColor        = Color.Magenta;
+    static public readonly Color       ZoneStorageOutlineColor = Color.SkyBlue;
+    static public readonly Color         ZoneOtherOutlineColor = Color.Magenta;
 
-    static public readonly Color CarBrandOutlineColor         = Color.Cyan;
+    static public readonly Color          CarBrandOutlineColor = Color.Cyan;
 
-    static public readonly Color CarBrandFillColor_Full        = Color.Red;
-    static public readonly Color CarBrandFillColor_AlmostFull  = Color.OrangeRed;
-    static public readonly Color CarBrandFillColor_AboveHalf   = Color.Orange;
-    static public readonly Color CarBrandFillColor_BelowHalf   = Color.Yellow;
+    static public readonly Color        CarBrandFillColor_Full = Color.Red;
+    static public readonly Color  CarBrandFillColor_AlmostFull = Color.OrangeRed;
+    static public readonly Color   CarBrandFillColor_AboveHalf = Color.Orange;
+    static public readonly Color   CarBrandFillColor_BelowHalf = Color.Yellow;
     static public readonly Color CarBrandFillColor_AlmostEmpty = Color.LimeGreen;
-    static public readonly Color CarBrandFillColor_Empty       = Color.Transparent;
+    static public readonly Color       CarBrandFillColor_Empty = Color.Transparent;
 
 
 
     // Dynamic settings
-    public const           string SettingsFileName  = "Settings.ukvp"; // ukvp == Unordered key-value pairs
-    static public readonly string SettingsFilePath  = Path.Combine(SolutionPath, SettingsFileName);
+    public const           string  SettingsFileName = "Settings.ukvp"; // ukvp == Unordered key-value pairs
+    static public readonly string  SettingsFilePath = Path.Combine(SolutionPath, SettingsFileName);
     public const           char   SettingsSeparator = ':';
   }
 }
---
+++ Core/UI/Colors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Core.UI {
+  public struct Colors {
+    public static Color Color100 { get => Color.Red; }
+    public static Color Color99_75 { get => Color.FromArgb(255, 130, 0); }
+    public static Color Color74_50 { get => Color.FromArgb(255, 200, 0); }
+    public static Color Color49_25 { get => Color.Yellow; }
+    public static Color Color24_1 { get => Color.Lime; }
+    public static Color Color0 { get => Color.Transparent; }
+  }
+}
