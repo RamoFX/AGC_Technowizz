f06a6a28ddca5393507398acbd2ed9c4255f83f9
Roman Dvorovoy <roman.dvorovoy123@gmail.com>
2022-02-05 16:05:04 +0100

    update: new example layout names

--- Core/DynamicSettings.cs
+++ Core/DynamicSettings.cs
@@ -4,7 +4,7 @@
 
 namespace Core {
   static public partial class DynamicSettings {
-    static public DynamicSetting ZA_StartupLayoutName = new("ZA_StartupLayoutName", "Example2");
+    static public DynamicSetting ZA_StartupLayoutName = new("ZA_StartupLayoutName", "Příklad 2"); // Až bude rozvržení "Příklad 1", tak změnit na něj
     static public DynamicSetting ZA_HighlightTimeOn = new("ZA_HighlightTimeOn", "600");
     static public DynamicSetting ZA_HighlightTimeOff = new("ZA_HighlightTimeOff", "400");
     static public DynamicSetting ZA_TotalHighlightFlashes = new("ZA_TotalHighlightFlashes", "4");
