7d3a3676bf0e4b8361c71e648187f475a1399773
Roman Dvorovoy <roman.dvorovoy123@gmail.com>
2022-02-06 15:06:10 +0100

    refactor: simpler naming

--- Core/DynamicSettings.cs
+++ Core/DynamicSettings.cs
@@ -4,25 +4,25 @@
 
 namespace Core {
   static public partial class DynamicSettings {
-    static public DynamicSetting ZA_StartupLayoutName          = new("ZA_StartupLayoutName", "Příklad 2"); // Až bude rozvržení "Příklad 1", tak změnit na něj
-    static public DynamicSetting ZA_HighlightTimeOn            = new("ZA_HighlightTimeOn", "600");
-    static public DynamicSetting ZA_HighlightTimeOff           = new("ZA_HighlightTimeOff", "400");
-    static public DynamicSetting ZA_TotalHighlightFlashes      = new("ZA_TotalHighlightFlashes", "4");
-    static public DynamicSetting ZA_LastHighlightOnOff         = new("ZA_LastHighlightOnOff", "true");
+    // Zone assigner
+    static public DynamicSetting     ZA_StartupLayoutName = new("ZA_StartupLayoutName",     "Příklad 1");
+    static public DynamicSetting       ZA_HighlightTimeOn = new("ZA_HighlightTimeOn",       "600");
+    static public DynamicSetting      ZA_HighlightTimeOff = new("ZA_HighlightTimeOff",      "400");
+    static public DynamicSetting ZA_TotalHighlightFlashes = new("ZA_TotalHighlightFlashes", "4");
+    static public DynamicSetting    ZA_LastHighlightOnOff = new("ZA_LastHighlightOnOff",    "true");
 
-    static public DynamicSetting LayoutOutlineColor            = new("LayoutOutlineColor", "@169,169,169");
-    static public DynamicSetting LayoutGridColor               = new("LayoutGridColor", "@211,211,211");
+    // Colors
+    static public DynamicSetting               LayoutColor = new("LayoutOutlineColor",            "@169,169,169");
+    static public DynamicSetting                 GridColor = new("LayoutGridColor",               "@211,211,211");
 
-    static public DynamicSetting ZoneOutlineColor_Storage      = new("ZoneOutlineColor_Storage", "@135,206,235");
-    static public DynamicSetting ZoneOutlineColor_Other        = new("ZoneOutlineColor_Other", "@255,0,255");
+    static public DynamicSetting         ZoneColor_Storage = new("ZoneOutlineColor_Storage",      "@135,206,235");
+    static public DynamicSetting           ZoneColor_Other = new("ZoneOutlineColor_Other",        "@255,0,255");
 
-    static public DynamicSetting CarBrandOutlineColor          = new("CarBrandOutlineColor", "@0,255,255");
-
-    static public DynamicSetting CarBrandFillColor_Full        = new("CarBrandFillColor_Full", "Red");
-    static public DynamicSetting CarBrandFillColor_AlmostFull  = new("CarBrandFillColor_AlmostFull", "@255,69,0");
-    static public DynamicSetting CarBrandFillColor_AboveHalf   = new("CarBrandFillColor_AboveHalf", "@255,165,0");
-    static public DynamicSetting CarBrandFillColor_BelowHalf   = new("CarBrandFillColor_BelowHalf", "Yellow");
-    static public DynamicSetting CarBrandFillColor_AlmostEmpty = new("CarBrandFillColor_AlmostEmpty", "LimeGreen");
-    static public DynamicSetting CarBrandFillColor_Empty       = new("CarBrandFillColor_Empty", "#ffff");
+    static public DynamicSetting        CarBrandColor_Full = new("CarBrandFillColor_Full",        "Red");
+    static public DynamicSetting  CarBrandColor_AlmostFull = new("CarBrandFillColor_AlmostFull",  "@255,69,0");
+    static public DynamicSetting   CarBrandColor_AboveHalf = new("CarBrandFillColor_AboveHalf",   "@255,165,0");
+    static public DynamicSetting   CarBrandColor_BelowHalf = new("CarBrandFillColor_BelowHalf",   "Yellow");
+    static public DynamicSetting CarBrandColor_AlmostEmpty = new("CarBrandFillColor_AlmostEmpty", "LimeGreen");
+    static public DynamicSetting       CarBrandColor_Empty = new("CarBrandFillColor_Empty",       "#ffff");
   }
 }
--- Core/UI/Dialogs/Help.cs
+++ Core/UI/Dialogs/Help.cs
@@ -19,12 +19,12 @@ namespace Core.UI.Dialogs
 
     private void Help_Load(object sender, EventArgs e)
     {
-      Color100_PictureBox.BackColor = Colors.FromStringToColor(DynamicSettings.CarBrandFillColor_Full.Value);
-      Color99_75_PictureBox.BackColor = Colors.FromStringToColor(DynamicSettings.CarBrandFillColor_AlmostFull.Value);
-      Color74_50_PictureBox.BackColor = Colors.FromStringToColor(DynamicSettings.CarBrandFillColor_AboveHalf.Value);
-      Color49_25_PictureBox.BackColor = Colors.FromStringToColor(DynamicSettings.CarBrandFillColor_BelowHalf.Value);
-      Color24_1_PictureBox.BackColor = Colors.FromStringToColor(DynamicSettings.CarBrandFillColor_AlmostEmpty.Value);
-      Color0_PictureBox.BackColor = Colors.FromStringToColor(DynamicSettings.CarBrandFillColor_Empty.Value);
+      Color100_PictureBox.BackColor = Colors.FromStringToColor(DynamicSettings.CarBrandColor_Full.Value);
+      Color99_75_PictureBox.BackColor = Colors.FromStringToColor(DynamicSettings.CarBrandColor_AlmostFull.Value);
+      Color74_50_PictureBox.BackColor = Colors.FromStringToColor(DynamicSettings.CarBrandColor_AboveHalf.Value);
+      Color49_25_PictureBox.BackColor = Colors.FromStringToColor(DynamicSettings.CarBrandColor_BelowHalf.Value);
+      Color24_1_PictureBox.BackColor = Colors.FromStringToColor(DynamicSettings.CarBrandColor_AlmostEmpty.Value);
+      Color0_PictureBox.BackColor = Colors.FromStringToColor(DynamicSettings.CarBrandColor_Empty.Value);
     }
   }
 }
