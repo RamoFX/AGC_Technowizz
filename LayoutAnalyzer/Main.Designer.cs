34097103e556dd51bcaa278adb0c27ad321b9cd6
Roman Dvorovoy <roman.dvorovoy123@gmail.com>
2022-02-06 18:04:12 +0100

    refactor: re-created LayoutDesigner with deletion of non-needed code

--- LayoutAnalyzer/Main.Designer.cs
+++ LayoutAnalyzer/Main.Designer.cs
@@ -1,70 +1,179 @@
-﻿namespace LayoutAnalyzer
-{
-  partial class Main
-  {
+﻿
+namespace LayoutAnalyzer {
+  partial class Main {
     /// <summary>
-    /// Vyžaduje se proměnná návrháře.
+    /// Required designer variable.
     /// </summary>
     private System.ComponentModel.IContainer components = null;
 
     /// <summary>
-    /// Uvolněte všechny používané prostředky.
+    /// Clean up any resources being used.
     /// </summary>
-    /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
-    protected override void Dispose(bool disposing)
-    {
-      if (disposing && (components != null))
-      {
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && ( components != null )) {
         components.Dispose();
       }
       base.Dispose(disposing);
     }
 
-    #region Kód generovaný Návrhářem Windows Form
+    #region Windows Form Designer generated code
 
     /// <summary>
-    /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
-    /// obsah této metody v editoru kódu.
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
     /// </summary>
-    private void InitializeComponent()
-    {
-      this.panel1 = new System.Windows.Forms.Panel();
-      this.panelProperties = new System.Windows.Forms.Panel();
+    private void InitializeComponent() {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+      this.MenuStrip = new System.Windows.Forms.MenuStrip();
+      this.ToolStripMenuItem_File = new System.Windows.Forms.ToolStripMenuItem();
+      this.ToolStripMenuItem_Open = new System.Windows.Forms.ToolStripMenuItem();
+      this.ToolStripComboBox_ImportableLayoutNames = new System.Windows.Forms.ToolStripComboBox();
+      this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+      this.ToolStripMenuItem_OpenInFileExplorer = new System.Windows.Forms.ToolStripMenuItem();
+      this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+      this.ToolStripMenuItem_Exit = new System.Windows.Forms.ToolStripMenuItem();
+      this.TreeView_Layout = new System.Windows.Forms.TreeView();
+      this.PictureBox_Layout = new System.Windows.Forms.PictureBox();
+      this.ToolStripMenuItem_Close = new System.Windows.Forms.ToolStripMenuItem();
+      this.MenuStrip.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Layout)).BeginInit();
       this.SuspendLayout();
       // 
-      // panel1
+      // MenuStrip
       // 
-      this.panel1.Location = new System.Drawing.Point(239, 12);
-      this.panel1.Name = "panel1";
-      this.panel1.Size = new System.Drawing.Size(481, 426);
-      this.panel1.TabIndex = 0;
-      this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+      this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem_File});
+      this.MenuStrip.Location = new System.Drawing.Point(0, 0);
+      this.MenuStrip.Name = "MenuStrip";
+      this.MenuStrip.Padding = new System.Windows.Forms.Padding(0);
+      this.MenuStrip.Size = new System.Drawing.Size(1067, 24);
+      this.MenuStrip.TabIndex = 1;
       // 
-      // panelProperties
+      // ToolStripMenuItem_File
       // 
-      this.panelProperties.Location = new System.Drawing.Point(13, 13);
-      this.panelProperties.Name = "panelProperties";
-      this.panelProperties.Size = new System.Drawing.Size(220, 425);
-      this.panelProperties.TabIndex = 1;
+      this.ToolStripMenuItem_File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem_Open,
+            this.ToolStripMenuItem_Close,
+            this.toolStripSeparator2,
+            this.ToolStripMenuItem_OpenInFileExplorer,
+            this.toolStripSeparator3,
+            this.ToolStripMenuItem_Exit});
+      this.ToolStripMenuItem_File.Name = "ToolStripMenuItem_File";
+      this.ToolStripMenuItem_File.Size = new System.Drawing.Size(57, 24);
+      this.ToolStripMenuItem_File.Text = "Soubor";
+      // 
+      // ToolStripMenuItem_Open
+      // 
+      this.ToolStripMenuItem_Open.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+      this.ToolStripMenuItem_Open.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripComboBox_ImportableLayoutNames});
+      this.ToolStripMenuItem_Open.Name = "ToolStripMenuItem_Open";
+      this.ToolStripMenuItem_Open.Size = new System.Drawing.Size(297, 22);
+      this.ToolStripMenuItem_Open.Text = "Otevřít";
+      // 
+      // ToolStripComboBox_ImportableLayoutNames
+      // 
+      this.ToolStripComboBox_ImportableLayoutNames.DropDownWidth = 200;
+      this.ToolStripComboBox_ImportableLayoutNames.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.ToolStripComboBox_ImportableLayoutNames.Name = "ToolStripComboBox_ImportableLayoutNames";
+      this.ToolStripComboBox_ImportableLayoutNames.Size = new System.Drawing.Size(200, 23);
+      // 
+      // toolStripSeparator2
+      // 
+      this.toolStripSeparator2.Name = "toolStripSeparator2";
+      this.toolStripSeparator2.Size = new System.Drawing.Size(294, 6);
+      // 
+      // ToolStripMenuItem_OpenInFileExplorer
+      // 
+      this.ToolStripMenuItem_OpenInFileExplorer.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+      this.ToolStripMenuItem_OpenInFileExplorer.Name = "ToolStripMenuItem_OpenInFileExplorer";
+      this.ToolStripMenuItem_OpenInFileExplorer.Size = new System.Drawing.Size(297, 22);
+      this.ToolStripMenuItem_OpenInFileExplorer.Text = "Všechna rozvržení v průzkumníku souborů";
+      this.ToolStripMenuItem_OpenInFileExplorer.Click += new System.EventHandler(this.ToolStripMenuItem_OpenInFileExplorer_Click);
+      // 
+      // toolStripSeparator3
+      // 
+      this.toolStripSeparator3.Name = "toolStripSeparator3";
+      this.toolStripSeparator3.Size = new System.Drawing.Size(294, 6);
+      // 
+      // ToolStripMenuItem_Exit
+      // 
+      this.ToolStripMenuItem_Exit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+      this.ToolStripMenuItem_Exit.Name = "ToolStripMenuItem_Exit";
+      this.ToolStripMenuItem_Exit.Size = new System.Drawing.Size(297, 22);
+      this.ToolStripMenuItem_Exit.Text = "Ukončit";
+      this.ToolStripMenuItem_Exit.Click += new System.EventHandler(this.ToolStripMenuItem_Exit_Click);
+      // 
+      // TreeView_Layout
+      // 
+      this.TreeView_Layout.LabelEdit = true;
+      this.TreeView_Layout.Location = new System.Drawing.Point(0, 24);
+      this.TreeView_Layout.Margin = new System.Windows.Forms.Padding(0);
+      this.TreeView_Layout.Name = "TreeView_Layout";
+      this.TreeView_Layout.PathSeparator = ".";
+      this.TreeView_Layout.ShowNodeToolTips = true;
+      this.TreeView_Layout.Size = new System.Drawing.Size(200, 546);
+      this.TreeView_Layout.TabIndex = 0;
+      // 
+      // PictureBox_Layout
+      // 
+      this.PictureBox_Layout.BackColor = System.Drawing.Color.White;
+      this.PictureBox_Layout.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.PictureBox_Layout.ErrorImage = null;
+      this.PictureBox_Layout.InitialImage = null;
+      this.PictureBox_Layout.Location = new System.Drawing.Point(199, 24);
+      this.PictureBox_Layout.Margin = new System.Windows.Forms.Padding(0);
+      this.PictureBox_Layout.Name = "PictureBox_Layout";
+      this.PictureBox_Layout.Size = new System.Drawing.Size(868, 546);
+      this.PictureBox_Layout.TabIndex = 2;
+      this.PictureBox_Layout.TabStop = false;
+      // 
+      // ToolStripMenuItem_Close
+      // 
+      this.ToolStripMenuItem_Close.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+      this.ToolStripMenuItem_Close.Name = "ToolStripMenuItem_Close";
+      this.ToolStripMenuItem_Close.Size = new System.Drawing.Size(297, 22);
+      this.ToolStripMenuItem_Close.Text = "Zavřít";
+      this.ToolStripMenuItem_Close.Click += new System.EventHandler(this.ToolStripMenuItem_Close_Click);
       // 
       // Main
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(732, 450);
-      this.Controls.Add(this.panelProperties);
-      this.Controls.Add(this.panel1);
+      this.ClientSize = new System.Drawing.Size(1067, 570);
+      this.Controls.Add(this.PictureBox_Layout);
+      this.Controls.Add(this.TreeView_Layout);
+      this.Controls.Add(this.MenuStrip);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MainMenuStrip = this.MenuStrip;
       this.Name = "Main";
-      this.Text = "Layout analyzer";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "(unset)";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
       this.Load += new System.EventHandler(this.Main_Load);
+      this.Paint += new System.Windows.Forms.PaintEventHandler(this.Main_Paint);
+      this.Resize += new System.EventHandler(this.Main_Resize);
+      this.MenuStrip.ResumeLayout(false);
+      this.MenuStrip.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.PictureBox_Layout)).EndInit();
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
-
-    private System.Windows.Forms.Panel panel1;
-    private System.Windows.Forms.Panel panelProperties;
+    private System.Windows.Forms.MenuStrip MenuStrip;
+    private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem_File;
+    private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem_Open;
+    private System.Windows.Forms.ToolStripComboBox ToolStripComboBox_ImportableLayoutNames;
+    private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+    private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem_OpenInFileExplorer;
+    private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+    private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem_Exit;
+    private System.Windows.Forms.TreeView TreeView_Layout;
+    private System.Windows.Forms.PictureBox PictureBox_Layout;
+    private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem_Close;
   }
 }
 
--- LayoutAnalyzer/Main.cs
+++ LayoutAnalyzer/Main.cs
@@ -1,74 +1,254 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using Core;
-using Core.UI;
 using Core.Storage;
+using Core.UI;
+using Core.UI.Dialogs;
 
-namespace LayoutAnalyzer
-{
-  public partial class Main : Form
-  {
-    Layout CurrentLayout;
 
-    public Main()
-    {
+
+namespace LayoutAnalyzer {
+
+  public partial class Main : Form {
+    // Constants
+    private const string TitleBase = "Analýza rozvržení";
+    private const string SelectBelow = "Vyberte níže...";
+
+
+
+    // Layout data
+    private Layout CurrentLayout;
+
+    private bool IsLayoutPresent {
+      get => this.CurrentLayout != null;
+    }
+
+    private List<string> ImportableLayoutsNames {
+      get => LayoutManager.GetExistingLayoutNames().Prepend(SelectBelow).ToList();
+    }
+
+
+
+    // Form and its Controls data
+    private int HeightOffset;
+
+
+
+    // Constructor
+    public Main() {
       InitializeComponent();
     }
 
-    private void Main_Load(object sender, EventArgs e)
-    {
-      SetCurrentLayout(Core.Storage.Layout.Import(DynamicSettings.ZA_StartupLayoutName.Value));
-      CurrentLayout.Initialize(true);
 
-      foreach (var zone in CurrentLayout.Zones)
-      {
-        foreach (var carBrand in zone.CarBrands)
-        {
-          panelProperties.Add(new EditableNode
-          {
-            LabelTitle = zone.Name,
-            Label = carBrand.Name,
-            DefaultValue = carBrand.PalletsCurrentlyStoredPercent.ToString(),
-          });
+
+    // Layout file system IO interactions
+    private void Unload(bool force) {
+      if (this.IsLayoutPresent) {
+        // If not saved ask user if he wants to save
+        // If forced then do not ask
+        if (!force && !this.CurrentLayout.IsCorrespondingFileUpToDate()) {
+          var dialogResult = MessageBoxes.SaveUnsavedLayout();
+
+          if (dialogResult == DialogResult.Yes) {
+            this.Save();
+          }
+        }
+
+        // Unload
+        this.CurrentLayout = null;
+
+        // Post-hooks
+        this.UpdateState();
+      }
+    }
+
+    private void Unload() {
+      this.Unload(false);
+    }
+
+    private void Open(string name) {
+      IEnumerable<string> validLayoutNames = LayoutManager.GetValidLayoutNames();
+
+      if (validLayoutNames.Contains(name)) {
+        Layout layout = Core.Storage.Layout.Import(name);
+        this.SetCurrentLayout(layout);
+      } else {
+        MessageBoxes.LayoutInvalid(name);
+      }
+    }
+
+    private void Save() {
+      if (this.IsLayoutPresent) {
+        // Save if not saved
+        if (!this.CurrentLayout.IsCorrespondingFileUpToDate()) {
+          this.CurrentLayout.Export();
+
+          // Post-hooks
+          this.UpdateState();
         }
       }
     }
 
 
-    private void panel1_Paint(object sender, PaintEventArgs e)
-    {
-      DrawLayout();
+
+
+    // State
+    private void UpdateTitle() {
+      if (this.IsLayoutPresent) {
+        this.Text = $"{ TitleBase } - { this.CurrentLayout.Name }";
+      } else {
+        this.Text = $"{ TitleBase }";
+      }
     }
 
-    private bool IsLayoutPresent
-    {
-      get => this.CurrentLayout != null;
+    private void UpdateOpenControl_Enabled() {
+      ToolStripMenuItem_Open.Enabled = this.ImportableLayoutsNames.Count() > 0;
     }
 
-    private void SetCurrentLayout(Layout layout)
-    {
+    private void UpdateCloseControl_Enabled() {
+      ToolStripMenuItem_Close.Enabled = this.IsLayoutPresent;
+    }
+
+    private void UpdateExistingLayouts_DataSource() {
+      this.ToolStripComboBox_ImportableLayoutNames.ComboBox.DataSource = ImportableLayoutsNames;
+
+      if (this.IsLayoutPresent) {
+        int index = ImportableLayoutsNames.FindIndex(value => value == this.CurrentLayout.Name);
+        this.ToolStripComboBox_ImportableLayoutNames.ComboBox.SelectedIndex = index == -1 ? 0 : index;
+      } else {
+        this.ToolStripComboBox_ImportableLayoutNames.ComboBox.SelectedIndex = 0;
+      }
+    }
+
+    private void UpdateState() {
+      this.UpdateTitle();
+      this.UpdateOpenControl_Enabled();
+      this.UpdateCloseControl_Enabled();
+      this.UpdateExistingLayouts_DataSource();
+
+      // Post-hooks
+      this.CurrentLayoutChangedHandler();
+    }
+
+    private void SetCurrentLayout(Layout layout) {
       this.CurrentLayout = layout;
 
-      if (this.IsLayoutPresent)
-      {
-        this.CurrentLayout.Initialize(false);
+      if (this.IsLayoutPresent) {
+        this.CurrentLayout.Initialize(true);
+      }
+
+      // Post-hooks
+      this.UpdateState();
+    }
+
+
+
+    // Visual
+    private void DrawLayout() {
+      if (this.IsLayoutPresent) {
+        Drawer.DrawLayout(this.PictureBox_Layout, this.CurrentLayout, new(SystemInformation.VerticalScrollBarWidth, 0));
       }
     }
 
-    private void DrawLayout()
-    {
-      if (this.IsLayoutPresent)
-      {
-        Drawer.DrawLayout(this.panel1, this.CurrentLayout, new Size(SystemInformation.VerticalScrollBarWidth, 0));
+
+
+    // Handlers
+    private void CurrentLayoutChangedHandler() {
+      // TreeView
+      this.TreeView_Layout.Nodes.Clear();
+
+      if (this.IsLayoutPresent) {
+        // Main
+        int Main_MinimumWidth = this.CurrentLayout.Size.Width * StaticSettings.UnitSize + this.TreeView_Layout.Width + SystemInformation.VerticalScrollBarWidth;
+        int Main_MinimumHeight = this.CurrentLayout.Size.Height * StaticSettings.UnitSize + this.HeightOffset;
+
+        this.MinimumSize = new Size(Main_MinimumWidth, Main_MinimumHeight);
+
+        // TreeView - jednotlivý statistiky
+        //var rootNode = TreeView_Layout.Nodes.Add(this.CurrentLayout.Name);
+
+        //foreach (Zone zone in this.CurrentLayout.Zones) {
+        //  var zoneNode = rootNode.Nodes.Add(zone.Name);
+
+        //  foreach (CarBrand carBrand in zone.CarBrands) {
+        //    zoneNode.Nodes.Add(carBrand.Name);
+        //  }
+        //}
+
+        this.TreeView_Layout.ExpandAll();
+
+        // Draw
+        this.DrawLayout();
+      } else {
+        // Set current form minimum size
+        this.MinimumSize = new Size(TreeView_Layout.Width + SystemInformation.VerticalScrollBarWidth, 256);
+
+        // Clear last drawing
+        this.PictureBox_Layout.CreateGraphics().Clear(this.PictureBox_Layout.BackColor);
       }
+    }
+
+
+
+    // MenuStrip children events
+    private void ToolStripComboBox_ImportableLayoutNames_ComboBox_SelectionChangeCommitted(object sender, EventArgs e) {
+      string existingLayoutName = (string) this.ToolStripComboBox_ImportableLayoutNames.SelectedItem;
+
+      if (!(existingLayoutName == SelectBelow)) {
+        this.Open(existingLayoutName);
+      }
+    }
+
+    private void ToolStripMenuItem_Close_Click(object sender, EventArgs e) {
+      this.Unload();
+    }
+
+    private void ToolStripMenuItem_OpenInFileExplorer_Click(object sender, EventArgs e) {
+      if (Directory.Exists(StaticSettings.LayoutsPath)) {
+        Process.Start("explorer.exe", StaticSettings.LayoutsPath);
+      } else {
+        MessageBoxes.NoLayoutsExist();
+      }
+    }
+
+    private void ToolStripMenuItem_Exit_Click(object sender, EventArgs e) {
+      this.Close();
+    }
+
+
+
+    // Main events
+    private void Main_Load(object sender, EventArgs e) {
+      this.ToolStripComboBox_ImportableLayoutNames.ComboBox.BindingContext = this.BindingContext;
+      this.ToolStripComboBox_ImportableLayoutNames.ComboBox.SelectionChangeCommitted += this.ToolStripComboBox_ImportableLayoutNames_ComboBox_SelectionChangeCommitted;
+      this.HeightOffset = this.Height - this.TreeView_Layout.Height;
+
+      // Post-hooks
+      this.SetCurrentLayout(null);
+      this.UpdateState();
+    }
+
+    private void Main_Resize(object sender, EventArgs e) {
+      this.TreeView_Layout.Height = this.Height - HeightOffset;
+
+      this.PictureBox_Layout.Width = this.Width - this.TreeView_Layout.Width;
+      this.PictureBox_Layout.Height = this.Height - this.HeightOffset;
+
+      this.DrawLayout();
+    }
+
+    private void Main_FormClosing(object sender, FormClosingEventArgs e) {
+      this.Unload();
+    }
+
+    private void Main_Paint(object sender, PaintEventArgs e) {
+      this.DrawLayout();
     }
   }
 }
