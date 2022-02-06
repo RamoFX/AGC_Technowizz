24869b92d72557b30105f12593e920d72c693b3f
ST3V1K <j.nekvind@seznam.cz>
2022-02-06 11:12:56 +0100

    add: start of TreeViewEditable

---
+++ Core/UI/CustomControls/TreeViewEditable.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Core.UI.CustomControls
+{
+  partial class TreeViewEditable
+  {
+    /// <summary> 
+    /// Vyžaduje se proměnná návrháře.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Uvolněte všechny používané prostředky.
+    /// </summary>
+    /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Kód vygenerovaný pomocí Návrháře komponent
+
+    /// <summary> 
+    /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+    /// obsah této metody v editoru kódu.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.SuspendLayout();
+      // 
+      // TreeViewEditable
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+      this.Name = "TreeViewEditable";
+      this.Size = new System.Drawing.Size(289, 399);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+  }
+}
---
+++ Core/UI/CustomControls/TreeViewEditable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Core.UI.CustomControls
+{
+  public partial class TreeViewEditable : UserControl
+  {
+    public IEnumerable<EditableNode> EditableNodes { get; private set; }
+
+    public TreeViewEditable()
+    {
+      InitializeComponent();
+      this.Controls.AddRange((Control[])this.EditableNodes);
+    }
+
+    public void Add(EditableNode item)
+    {
+      EditableNodes.Append(item);
+    }
+
+    public void Insert(EditableNode item, int index)
+    {
+      this.EditableNodes = this.EditableNodes
+                            .Take(index)
+                            .Append(item)
+                            .Concat(this.EditableNodes.Skip(index));
+    }
+
+    public void Remove(EditableNode item)
+    {
+      this.EditableNodes = this.EditableNodes.SkipWhile(editableNode => editableNode == item);
+    }
+
+    public void RemoveAt(int index)
+    {
+      this.EditableNodes = this.EditableNodes.SkipWhile(node => this.EditableNodes.ElementAt(index) == node);
+    }
+  }
+}
