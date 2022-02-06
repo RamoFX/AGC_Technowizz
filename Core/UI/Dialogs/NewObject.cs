b58fb27e2ca8a753a1700e68b67a5bc31a0b3c2d
Roman Dvorovoy <roman.dvorovoy123@gmail.com>
2022-02-06 23:58:53 +0100

    feature: dialog - user can create complex objects

---
+++ Core/UI/Dialogs/NewObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+
+namespace Core.UI.Dialogs {
+  public partial class NewObject : Form {
+    public new DialogResult DialogResult;
+    public Func<object, bool> ValueValidator;
+    public object FinalValue;
+
+
+
+    public NewObject(Func<object, bool> valueValidator, string labelText) {
+      InitializeComponent();
+      this.DialogResult = DialogResult.None;
+      this.ValueValidator = valueValidator;
+      this.Label.Text = labelText;
+    }
+
+
+
+    private void Button_Apply_Click(object sender, EventArgs e) {
+      object newObject = PropertyGrid.SelectedObject;
+      bool isValid = this.ValueValidator(newObject);
+
+      if (isValid) {
+        this.DialogResult = DialogResult.OK;
+        this.FinalValue = newObject;
+        this.Close();
+      }
+    }
+
+
+
+    private void Button_Cancel_Click(object sender, EventArgs e) {
+      this.DialogResult = DialogResult.Cancel;
+      this.Close();
+    }
+  }
+}
