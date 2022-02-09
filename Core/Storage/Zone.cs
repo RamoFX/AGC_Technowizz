782f879415d50568d190d0086980730c3616fa09
Roman Dvorovoy <roman.dvorovoy123@gmail.com>
2022-02-09 14:37:53 +0100

    refactor+feature(Core): better naming, Area, made IEnumerable (Zone)

--- Core/Storage/Zone.cs
+++ Core/Storage/Zone.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -10,30 +11,27 @@ using Core.Helpers;
 
 
 namespace Core.Storage {
-  public class Zone : XElementConvertable<Zone>, IStorageMember {
-    // Main
-    [Browsable(true)]
-    [DisplayName("Název")]
+  public class Zone : XElementConvertable<Zone>, IEnumerable<CarBrand> {
+    [Browsable(true), DisplayName("Název")]
     public string Name { get; set; }
 
-    [Browsable(true)]
-    [DisplayName("Typ")]
+
+
+    [Browsable(true), DisplayName("Typ")]
     [Description("Storage - zóna určena pro uložení palet. Other - jiné využití zóny (např. zóna exportu).")]
     public ZoneType Type { get; set; }
 
-    [Browsable(false)]
-    public List<CarBrand> CarBrands;
 
 
-
-    // IVisualizable
-    [Browsable(true)]
-    [DisplayName("Souřadnice")]
+    [Browsable(true), DisplayName("Souřadnice")]
     public Point Location { get; set; }
 
-    [Browsable(true)]
-    [DisplayName("Rozměry")]
+
+
+    [Browsable(true), DisplayName("Rozměry")]
     public Size Size { get; set; }
+
+
 
     [Browsable(false)]
     public Rectangle Rectangle {
@@ -51,41 +49,61 @@ namespace Core.Storage {
       }
     }
 
+    [Browsable(false)]
+    public List<CarBrand> CarBrands;
 
 
-    // Computations
+
+    // Data
+    [Browsable(false)]
+    public int Area {
+      get => this.Size.Width * this.Size.Height;
+    }
+
+    [Browsable(false)]
+    public int Area_CarBrands {
+      get => this.Sum(carBrand => carBrand.Area);
+    }
+
     [Browsable(false)]
     public int MaxCapacity {
-      get => this.CarBrands.Sum(carBrand => carBrand.MaxCapacity);
+      get => this.Sum(carBrand => carBrand.MaxCapacity);
     }
 
     [Browsable(false)]
-    public int PalletsCurrentlyStored {
-      get => this.CarBrands.Sum(carBrand => carBrand.PalletsCurrentlyStored);
+    public int Stored {
+      get => this.Sum(carBrand => carBrand.Stored);
     }
 
     [Browsable(false)]
-    public int PalletsCurrentlyStoredPercent {
+    public int StoredPercent {
       get {
-        if (this.CarBrands.Count == 0)
+        if (this.Count() == 0)
           return 0;
 
-        double average = this.CarBrands.Average(carBrand => carBrand.PalletsCurrentlyStoredPercent);
+        double average = this.Average(carBrand => carBrand.StoredPercent);
 
         return (int) Math.Round(average);
       }
     }
 
     [Browsable(false)]
-    public int PalletsCanBeStored {
-      get => this.MaxCapacity - this.PalletsCurrentlyStored;
+    public int CanStore {
+      get => this.Sum(carBrand => carBrand.CanStore);
     }
 
 
 
-    // Other
-    private Layout LayoutParent;
-    private bool UseDatabaseAccess;
+    // Indexer
+    public CarBrand this[int index] {
+      get => this.CarBrands[index];
+    }
+
+
+
+    // References
+    [Browsable(false)]
+    internal Layout Parent;
 
 
 
@@ -104,19 +122,23 @@ namespace Core.Storage {
 
 
     // Initializators
-    public void Initialize(Layout layoutParent, bool useDatabaseAccess) {
-      this.LayoutParent = layoutParent;
-      this.UseDatabaseAccess = useDatabaseAccess;
+    public void Initialize(Layout parent) {
+      this.Parent = parent;
 
-      foreach (CarBrand carBrand in this.CarBrands) {
-        carBrand.Initialize(this.LayoutParent, this, this.UseDatabaseAccess);
+      foreach (CarBrand carBrand in this) {
+        carBrand.Initialize(this);
       }
     }
 
 
 
     // Self interaction
-    public CarBrand GetCarBrand(string name) {
+    public void Add(CarBrand carBrand) {
+      carBrand.Initialize(this);
+      this.CarBrands.Add(carBrand);
+    }
+
+    public CarBrand GetCarBrandOrDefault(string name) {
       return this.CarBrands.FirstOrDefault(carBrand => carBrand.Name == name);
     }
 
@@ -124,15 +146,14 @@ namespace Core.Storage {
 
     // CarBrand interaction
     private bool IsSuitable(string carBrandName) {
-      return this.GetCarBrand(carBrandName) != default;
+      return this.GetCarBrandOrDefault(carBrandName) != default;
     }
 
     public bool IsLoadable(string carBrandName) {
-      if (this.IsSuitable(carBrandName)) {
-        return this.GetCarBrand(carBrandName).PalletsCanBeStored > 0;
-      } else {
+      if (!this.IsSuitable(carBrandName))
         return false;
-      }
+
+      return this.GetCarBrandOrDefault(carBrandName).CanStore > 0;
     }
 
 
@@ -163,19 +184,15 @@ namespace Core.Storage {
 
 
 
-    // ICloneable
-    public object Clone() {
-      Zone zone = (Zone) this.MemberwiseClone();
+    // IEnumerable
+    public IEnumerator<CarBrand> GetEnumerator() {
+      foreach (CarBrand carBrand in this.CarBrands) {
+        yield return carBrand;
+      }
+    }
 
-      zone.Name = this.Name;
-      zone.Location = this.Location;
-      zone.Size = this.Size;
-      zone.Type = this.Type;
-      zone.CarBrands = this.CarBrands;
-
-      zone.Initialize(this.LayoutParent, this.UseDatabaseAccess);
-
-      return zone;
+    IEnumerator IEnumerable.GetEnumerator() {
+      return this.GetEnumerator();
     }
   }
 }
