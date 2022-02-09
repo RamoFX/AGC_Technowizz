7d8d52381b84660b27306b685768de7a87ba6662
Roman Dvorovoy <roman.dvorovoy123@gmail.com>
2022-02-09 14:39:36 +0100

    update(Core): unit size bigger (StaticSettings)

--- Core/StaticSettings.cs
+++ Core/StaticSettings.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Drawing;
 using System.IO;
 
 
@@ -8,26 +7,26 @@ namespace Core {
   public static class StaticSettings {
     // Solution
     static public readonly string SolutionBasePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-    public const           string SolutionBaseName = "AGC_Technowizz";
-    static public readonly string     SolutionPath = Path.Combine(SolutionBasePath, SolutionBaseName);
+    public const string SolutionBaseName = "AGC_Technowizz";
+    static public readonly string SolutionPath = Path.Combine(SolutionBasePath, SolutionBaseName);
 
 
 
     // Layout
-    public const           string LayoutsBaseName = "Layouts";
-    static public readonly string     LayoutsPath = Path.Combine(SolutionPath, LayoutsBaseName);
+    public const string LayoutsBaseName = "Layouts";
+    static public readonly string LayoutsPath = Path.Combine(SolutionPath, LayoutsBaseName);
 
 
 
     // Visual
-    public const           int                    UnitSize = 30;
-    public const           int                 OutlineSize = 4;
+    public const int UnitSize = 48;
+    public const int OutlineSize = 4;
 
 
     // Dynamic settings
-    public const           string  SettingsFileName = "Settings.ukvp"; // ukvp == Unordered key-value pairs
-    static public readonly string  SettingsFilePath = Path.Combine(SolutionPath, SettingsFileName);
-    public const           char   SettingsSeparator = ':';
+    public const string SettingsFileName = "Settings.ukvp"; // ukvp == Unordered key-value pairs
+    static public readonly string SettingsFilePath = Path.Combine(SolutionPath, SettingsFileName);
+    public const char SettingsSeparator = ':';
 
 
 
