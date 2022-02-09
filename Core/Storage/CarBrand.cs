ecf8858a26d42ec262ba4a8b98aa2d2516572121
Roman Dvorovoy <roman.dvorovoy123@gmail.com>
2022-02-09 14:37:10 +0100

    refactor+optimization+feature(Core): Com.DatAcc calls optimization, better naming, Area (CarBrand)

--- Core/Storage/CarBrand.cs
+++ Core/Storage/CarBrand.cs
@@ -10,23 +10,28 @@ using Core.Helpers;
 
 
 namespace Core.Storage {
-  public class CarBrand : XElementConvertable<CarBrand>, IStorageMember {
-    // Main
+  public class CarBrand : XElementConvertable<CarBrand> {
     [Browsable(true)]
     [DisplayName("Značka auta")]
     public string Name { get; set; }
 
 
 
-    // IVisualizable
-    [Browsable(true)]
-    [DisplayName("Absolutní souřadnice")]
-    [Description("Souřadnice vůči rozvržení.")]
+    [Browsable(true), DisplayName("Relativní souřadnice")]
+    [Description("Souřadnice vůči dané zóně.")]
     public Point Location { get; set; }
 
-    [Browsable(true)]
-    [DisplayName("Rozměry")]
+
+
+    [Browsable(true), DisplayName("Rozměry")]
     public Size Size { get; set; }
+
+
+
+    [Browsable(true), DisplayName("Vertikální kapacita")]
+    public int VerticalCapacity { get; set; }
+
+
 
     [Browsable(false)]
     public Rectangle Rectangle {
@@ -34,19 +39,17 @@ namespace Core.Storage {
     }
 
     [Browsable(false)]
-    public Color Color { // Doesn't return right color because it does only first time: Drawer inside create a lot of copies that have disallowed using C.DA, so output is after only empty
+    public Color Color {
       get {
-        int palletsCurrentlyStoredPercent = this.PalletsCurrentlyStoredPercent;
-
-        if (palletsCurrentlyStoredPercent == 100) {
+        if (this.StoredPercent == 100) {
           return DynamicSettings.CarBrandColor_Full.Value.ToColor();
-        } else if (palletsCurrentlyStoredPercent < 100 && this.PalletsCurrentlyStoredPercent >= 75) {
+        } else if (this.StoredPercent < 100 && this.StoredPercent >= 75) {
           return DynamicSettings.CarBrandColor_AlmostFull.Value.ToColor();
-        } else if (palletsCurrentlyStoredPercent < 75 && this.PalletsCurrentlyStoredPercent >= 50) {
+        } else if (this.StoredPercent < 75 && this.StoredPercent >= 50) {
           return DynamicSettings.CarBrandColor_AboveHalf.Value.ToColor();
-        } else if (palletsCurrentlyStoredPercent < 50 && this.PalletsCurrentlyStoredPercent >= 25) {
+        } else if (this.StoredPercent < 50 && this.StoredPercent >= 25) {
           return DynamicSettings.CarBrandColor_BelowHalf.Value.ToColor();
-        } else if (palletsCurrentlyStoredPercent < 25 && this.PalletsCurrentlyStoredPercent > 0) {
+        } else if (this.StoredPercent < 25 && this.StoredPercent > 0) {
           return DynamicSettings.CarBrandColor_AlmostEmpty.Value.ToColor();
         } else {
           return DynamicSettings.CarBrandColor_Empty.Value.ToColor();
@@ -56,63 +59,66 @@ namespace Core.Storage {
 
 
 
-    // Computations
+    // Data
+    [Browsable(false)]
+    public int Area {
+      get => this.Size.Width * this.Size.Height;
+    }
+
     [Browsable(false)]
     public int MaxCapacity {
-      get => this.Size.Width * this.Size.Height * this.LayoutParent.VerticalCapacity;
+      get => this.Area * this.VerticalCapacity;
     }
 
     [Browsable(false)]
-    public int PalletsCurrentlyStored {
-      get {
-        if (this.UseDatabaseAccess) {
-          return DatabaseAccess.GetZonePalletsCount(this.LayoutParent.WarehouseName, this.ZoneParent.Name, this.Name);
-        } else {
-          return 0;
-        }
-      }
+    public int Stored;
+
+    [Browsable(false)]
+    public int StoredPercent {
+      get => (int) Math.Round((decimal) this.Stored / this.MaxCapacity * 100);
     }
 
     [Browsable(false)]
-    public int PalletsCurrentlyStoredPercent {
-      get => (int) Math.Round((decimal) this.PalletsCurrentlyStored / this.MaxCapacity * 100);
+    public int CanStore {
+      get => this.MaxCapacity - this.Stored;
     }
 
+
+
+    // References
     [Browsable(false)]
-    public int PalletsCanBeStored {
-      get => this.MaxCapacity - this.PalletsCurrentlyStored;
-    }
+    internal Zone Parent;
 
 
-
-    // Other
-    private Layout LayoutParent;
-    private Zone ZoneParent;
-    private bool UseDatabaseAccess = false;
-
-    
 
     // Constructors
-    public CarBrand(string name, Point location, Size size) {
+    public CarBrand(string name, Point location, Size size, int verticalCapacity) {
       this.Name = name;
       this.Location = location;
       this.Size = size;
+      this.VerticalCapacity = verticalCapacity;
     }
 
 
 
     // Initializators
-    public void Initialize(Layout layoutParent, Zone zoneParent, bool useDatabaseAccess) {
-      this.LayoutParent = layoutParent;
-      this.ZoneParent = zoneParent;
-      this.UseDatabaseAccess = useDatabaseAccess;
+    public void Initialize(Zone parent) {
+      this.Parent = parent;
+
+      string zoneName = this.Parent.Name;
+
+      Layout layout = this.Parent.Parent;
+      string warehouseName = layout.WarehouseName;
+      int daysPeriod = layout.DaysPeriod;
+
+      this.Stored = DatabaseAccess.GetPalletsCount(warehouseName, zoneName, this.Name, daysPeriod);
     }
 
 
 
     // Self interaction
     public bool HasAvailableSpace() {
-      return this.PalletsCanBeStored > 0;
+      return this.CanStore > 0;
     }
 
 
@@ -122,7 +128,8 @@ namespace Core.Storage {
       XAttribute[] attributes = {
         new("name", this.Name),
         new("location", this.Location.ToCustomString()),
-        new("size", this.Size.ToCustomString())
+        new("size", this.Size.ToCustomString()),
+        new("verticalCapacity", this.VerticalCapacity.ToString())
       };
 
       return XmlLinqUtilities.CreateElement("CarBrand", attributes);
@@ -132,23 +139,9 @@ namespace Core.Storage {
       string name = element.Attribute("name").Value;
       Point location = element.Attribute("location").Value.ToPoint();
       Size size = element.Attribute("size").Value.ToSize();
+      int verticalCapacity = element.Attribute("verticalCapacity").Value.ToInt();
 
-      return new(name, location, size);
-    }
-
-
-
-    // ICloneable
-    public object Clone() {
-      CarBrand carBrand = (CarBrand) this.MemberwiseClone();
-
-      carBrand.Name = this.Name;
-      carBrand.Location = this.Location;
-      carBrand.Size = this.Size;
-
-      carBrand.Initialize(this.LayoutParent, this.ZoneParent, this.UseDatabaseAccess);
-
-      return carBrand;
+      return new(name, location, size, verticalCapacity);
     }
   }
 }
