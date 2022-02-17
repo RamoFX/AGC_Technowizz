0a1cf641263b38d8c28cdf33aba6c1f9e5537ee0
Roman Dvorovoy <roman.dvorovoy123@gmail.com>
2022-02-17 22:10:27 +0100

    cleanup(Core): Drawer no unused or dedudants

--- Core/UI/Drawer.cs
+++ Core/UI/Drawer.cs
@@ -12,9 +12,7 @@ using Core.Settings;
 
 namespace Core.UI {
   public static class Drawer {
-    static private readonly int UnitSize = StaticSettings.UNIT_SIZE;
-    static private readonly int OutlineSize = StaticSettings.OUTLINE_SIZE;
-    static private readonly Pen NeutralLight = DynamicSettings.GridColor.Value.ToColor().ToPen(OutlineSize);
+    static private readonly Pen NeutralLight = DynamicSettings.GridColor.Value.ToColor().ToPen(StaticSettings.OUTLINE_SIZE);
 
 
 
@@ -25,34 +23,34 @@ namespace Core.UI {
       // Vertical grid lines
       for (int x = 0; x <= layout.Size.Width; x++) {
         // Draw only if inside clip
-        if (!graphics.IsVisible(x * UnitSize, clip.Location.Y))
+        if (!graphics.IsVisible(x * StaticSettings.UNIT_SIZE, clip.Location.Y))
           continue;
 
         graphics.DrawLine(
           NeutralLight,
 
           new Point(x, 0)
-            .Scale(UnitSize),
+            .Scale(StaticSettings.UNIT_SIZE),
 
           new Point(x, layout.Size.Height)
-            .Scale(UnitSize)
+            .Scale(StaticSettings.UNIT_SIZE)
         );
       }
 
       // Horizontal grid lines
       for (int y = 0; y <= layout.Size.Height; y++) {
         // Draw only if inside clip
-        if (!graphics.IsVisible(clip.Location.X, y * UnitSize))
+        if (!graphics.IsVisible(clip.Location.X, y * StaticSettings.UNIT_SIZE))
           continue;
 
         graphics.DrawLine(
           NeutralLight,
 
           new Point(0, y)
-            .Scale(UnitSize),
+            .Scale(StaticSettings.UNIT_SIZE),
 
           new Point(layout.Size.Width, y)
-            .Scale(UnitSize)
+            .Scale(StaticSettings.UNIT_SIZE)
         );
       }
 
@@ -60,7 +58,7 @@ namespace Core.UI {
 
       // Zones
       foreach (Zone.Entity zone in layout.Zones) {
-        var zoneRectangle = zone.Rectangle.Scale(UnitSize);
+        var zoneRectangle = zone.Rectangle.Scale(StaticSettings.UNIT_SIZE);
 
         if (!graphics.IsVisible(zoneRectangle))
           continue;
@@ -71,7 +69,7 @@ namespace Core.UI {
             .Transparentize(),
 
           zone.Rectangle
-            .Scale(UnitSize)
+            .Scale(StaticSettings.UNIT_SIZE)
         );
 
         if (zone.VerticalCapacity == 0) {
