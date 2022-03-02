3fba69b073bf2ac099d331dfdf27f3a046e3f235
Roman Dvorovoy <roman.dvorovoy123@gmail.com>
2022-03-02 12:20:44 +0100

    refactor(Core, ZoneAssigner): Hight contrast layout drawing

--- Core/UI/Drawer.cs
+++ Core/UI/Drawer.cs
@@ -88,5 +88,77 @@ namespace Core.UI {
         graphics.DrawString(zone.Name, StaticSettings.ZoneNameFont, Brushes.White, zoneRectangle);
       }
     }
+
+
+
+    static public void DrawHightContrastLayout(Graphics graphics, Rectangle clip, int unitSize, Layout.Entity layout, object selection) {
+      // Current visible clip
+      graphics.SetClip(clip);
+
+
+
+      // Vertical grid lines
+      for (int x = 0; x <= layout.Size.Width; x++) {
+        // Draw only if inside visible clip
+        if (!graphics.IsVisible(x * unitSize, clip.Location.Y))
+          continue;
+
+        graphics.DrawLine(
+          NeutralLight,
+
+          new Point(x, 0)
+            .Scale(unitSize),
+
+          new Point(x, layout.Size.Height)
+            .Scale(unitSize)
+        );
+      }
+
+
+
+      // Horizontal grid lines
+      for (int y = 0; y <= layout.Size.Height; y++) {
+        // Draw only if inside visible clip
+        if (!graphics.IsVisible(clip.Location.X, y * unitSize))
+          continue;
+
+        graphics.DrawLine(
+          NeutralLight,
+
+          new Point(0, y)
+            .Scale(unitSize),
+
+          new Point(layout.Size.Width, y)
+            .Scale(unitSize)
+        );
+      }
+
+
+
+      // Zones
+      foreach (Zone.Entity zone in layout.Zones) {
+        var zoneRectangle = zone.Rectangle.Scale(unitSize);
+
+        // Draw only if inside visible clip
+        if (!graphics.IsVisible(zoneRectangle))
+          continue;
+
+        // Preparation
+        bool isSelection = zone == selection;
+
+        var brush = zone.Color
+            .ToBrush()
+            .Transparentize(isSelection ? 150 : 200);
+
+        var pen = zone.Color
+            .ToPen(StaticSettings.OUTLINE_SIZE);
+
+        // Draw
+        if (isSelection)
+          graphics.FillRectangle(brush, zoneRectangle);
+
+        graphics.DrawRectangle(pen, zoneRectangle);
+      }
+    }
   }
 }
